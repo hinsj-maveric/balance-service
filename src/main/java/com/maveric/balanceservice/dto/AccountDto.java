5d420986088fc9f561cc7ab93a78e0a9e26b2722
Hins Jain <hinsj@maveric-systems.com>
2023-02-27 12:23:09 +0530

    Maven clean install

--- src/main/java/com/maveric/balanceservice/dto/AccountDto.java
+++ src/main/java/com/maveric/balanceservice/dto/AccountDto.java
@@ -1,12 +1,16 @@
 package com.maveric.balanceservice.dto;
 
 import com.maveric.balanceservice.enums.Type;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class AccountDto {
     private String _id;
